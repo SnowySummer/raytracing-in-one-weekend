6b3cd5c0318fcce0ea4fad8c7ef8e096fdb9f26c
Quentin Patinier <quentin.patinier@gmail.com>
2026-06-04 15:19:19 +0700

    Moved scene setting to its own file

---
+++ scene/rtiow_scene_l.hpp
@@ -0,0 +1,32 @@
+#ifndef SCENE__RTIOW_SCENE_L_HPP
+#define SCENE__RTIOW_SCENE_L_HPP
+
+#include <Framebuffer.hpp>
+#include <Camera/Camera.hpp>
+#include <Camera/PinholeCamera.hpp>
+#include <Hittable/Hittable.hpp>
+#include <Hittable/HittableList.hpp>
+#include <Hittable/Sphere.hpp>
+#include <memory>
+
+void rtiow_scene1(
+    Framebuffer& framebuffer,
+    std::shared_ptr<Camera>& camera,
+    std::shared_ptr<Hittable>& world
+) {
+    // Setup framebuffer
+    framebuffer = Framebuffer(400, 225);
+
+    // Setup camera
+    std::shared_ptr<PinholeCamera> pinhole_camera = std::make_shared<PinholeCamera>();
+    pinhole_camera->setup(framebuffer);
+    camera = pinhole_camera;
+
+    // Setup scene
+    std::shared_ptr<HittableList> hittable_l = std::make_shared<HittableList>();
+    hittable_l->add(std::make_shared<Sphere>(vec4(0.0f, 0.0f, -1.0f), 0.5f));
+    hittable_l->add(std::make_shared<Sphere>(vec4(0.0f, -100.5f, -1.0f), 100.0f));
+    world = hittable_l;
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,15 @@
 #include <io/FramebufferExport.hpp>
 #include <memory>
 
+#include <../scene/rtiow_scene_l.hpp>
+
 int main() {
 
-    // Setup framebuffer
-    Framebuffer framebuffer = Framebuffer(400, 225);
-
-    // Setup camera
-    std::shared_ptr<Camera> camera;
-    std::shared_ptr<PinholeCamera> camera_impl = std::make_shared<PinholeCamera>();
-    camera_impl->setup(framebuffer);
-    camera = camera_impl;
-
     // Setup scene
+    Framebuffer framebuffer;
+    std::shared_ptr<Camera> camera;
     std::shared_ptr<Hittable> world;
-    std::shared_ptr<HittableList> hittable_l = std::make_shared<HittableList>();
-    hittable_l->add(std::make_shared<Sphere>(vec4(0.0f, 0.0f, -1.0f), 0.5f));
-    hittable_l->add(std::make_shared<Sphere>(vec4(0.0f, -100.5f, -1.0f), 100.0f));
-    world = hittable_l;
+    rtiow_scene1(framebuffer, camera, world);
 
     // Render
     Renderer renderer;
