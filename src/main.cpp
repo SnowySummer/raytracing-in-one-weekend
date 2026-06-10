c0c9be9d251cafa6488f19b8648b8fca701595b5
Quentin Patinier <quentin.patinier@gmail.com>
2026-06-10 16:16:08 +0700

    Update README.md

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@
 int main() {
 
     // Setup scene and render
-    std::shared_ptr<Scene> scene = std::make_shared<RTTROYL::Scene3>();
+    std::shared_ptr<Scene> scene = std::make_shared<RTTNW::Scene9>();
     scene->init_all();
     scene->render();
 
